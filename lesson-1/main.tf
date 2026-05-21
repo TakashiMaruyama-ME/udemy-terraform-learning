67f7dc8885b0b853e3c41a10f804663926741b2e
TakashiMaruyama-ME <tac_m@hotmail.com>
2026-05-20 20:18:08 -0700

    Remove deprecated GitHub provider configuration and add new random provider setup

---
+++ lesson-1/main.tf
@@ -0,0 +1,13 @@
+provider "random" {
+}
+variable "num_of_pets" {
+  type    = number
+  description = "Number of pets"
+}
+resource "random_pet" "name" {
+  length = var.num_of_pets
+}
+
+output "random_pet_name" {
+  value = random_pet.name.id
+}
